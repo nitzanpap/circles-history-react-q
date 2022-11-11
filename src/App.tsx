e9b4c8bbdbee839603f3d646de42e1de0ed294a6
nitzanpap <nitzanpap@gmail.com>
2022-11-11 04:08:08 +0200

    Added logo on click

--- src/App.tsx
+++ src/App.tsx
@@ -5,17 +5,42 @@ import AppLogo from './components/AppLogo/AppLogo';
 function App() {
   const [backwardsDisabled, setBackwardsDisabled] = useState(false);
   const [forwardDisabled, setForwardDisabled] = useState(true);
+  const [circleElsArr, setCircleElsArr]: any[] = useState([]);
+  const [currentIndexInHistory, setCurrentIndexInHistory]: any[] = useState(0);
+
+  const addCircleToScreen = (e: React.MouseEvent<HTMLElement>) => {
+    const newCoordinate = { x: e.clientX, y: e.clientY };
+    setCurrentIndexInHistory(currentIndexInHistory + 1);
+    console.log('Added circle ', currentIndexInHistory);
+    const newCircle = (
+      <AppLogo
+        id={`circle-${currentIndexInHistory}`}
+        key={currentIndexInHistory}
+        coordinate={newCoordinate}
+      />
+    );
+    setCircleElsArr([...circleElsArr, newCircle]);
+  };
+
+  const handleBackwards = () => {
+    setCircleElsArr([]);
+  };
+
   return (
-    <div className="App">
+    <div className="App" onClick={(e) => addCircleToScreen(e)}>
       <section className="history-btn-section">
-        <button className="history-btn backward-btn" disabled={backwardsDisabled}>
+        <button
+          className="history-btn backward-btn"
+          disabled={backwardsDisabled}
+          onClick={handleBackwards}
+        >
           Backward
         </button>
         <button className="history-btn forward-btn" disabled={forwardDisabled}>
           Forward
         </button>
       </section>
-      {/* <AppLogo posX={} posY={}/> */}
+      {circleElsArr}
     </div>
   );
 }
--- src/components/AppLogo/AppLogo.tsx
+++ src/components/AppLogo/AppLogo.tsx
@@ -2,10 +2,22 @@ import { FunctionComponent } from 'react';
 import logo from './logo.svg';
 import styles from './AppLogo.module.scss';
 
-interface AppLogoProps {}
+interface AppLogoProps {
+  coordinate: { x: number; y: number };
+  id: string;
+}
 
-const AppLogo: FunctionComponent<AppLogoProps> = () => {
-  return <img src={logo} className={styles.AppLogo} alt="logo" />;
+const AppLogo: FunctionComponent<AppLogoProps> = ({ coordinate, id }) => {
+  const height = 40;
+  return (
+    <img
+      id={id}
+      src={logo}
+      className={styles.AppLogo}
+      alt="logo"
+      style={{ height: height, left: coordinate.x - height / 2 - 8, top: coordinate.y - height / 2 }}
+    />
+  );
 };
 
 export default AppLogo;
